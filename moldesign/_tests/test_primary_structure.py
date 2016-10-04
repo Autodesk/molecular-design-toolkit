6439b9ed826bd9b8989b13c47fd4a773de250c88
Aaron Virshup <aaron.virshup@autodesk.com>
2016-10-03 20:42:25 -0700

    Mark expected test failures

--- moldesign/_tests/test_primary_structure.py
+++ moldesign/_tests/test_primary_structure.py
@@ -43,6 +43,7 @@ def test_3aid_cif_chains(protease_cif):
     assert mol.chains['D'].type == mol.chains['D'].type == 'water'
 
 
+@pytest.mark.xfail(reason='Known bug with biopython mmCIF parser, should be fixed before 0.7.4')
 def test_3aid_cif_separate_waters(protease_cif):
     mol = protease_cif
     assert mol.chains['D'].num_residues == 5
@@ -113,6 +114,8 @@ def test_residue_lookup_by_name_and_index(fixture, request):
 @pytest.mark.parametrize('fixture', fixture_types['protein'])
 def test_atom_lookup_by_name_and_index(fixture, request):
     mol = request.getfuncargvalue(fixture)
+    if mol.name.split('.')[-1] == 'cif':
+        pytest.xfail(reason='Known bug with biopython mmCIF parser, should be fixed before 0.7.4')
 
     for residue in mol.residues:
         for iatom, atom in enumerate(residue.atoms):
