fb76d368166c145db729dcf60e229a677ed1bc38
Aaron Virshup <aaron.virshup@autodesk.com>
2016-10-24 18:17:48 -0700

    ParmEd-parsed molecules now use the mmCIF chain IDs

--- moldesign/interfaces/__init__.py
+++ moldesign/interfaces/__init__.py
@@ -4,6 +4,7 @@ from . import nbo_interface
 from . import openbabel
 from . import openmm
 from . import opsin_interface
+from . import parmed_interface
 from . import pdbfixer_interface
 from . import pyscf_interface
 from . import symmol_interface
@@ -12,3 +13,4 @@ from .biopython_interface import *
 from .openbabel import *
 from .openmm import *
 from .pyscf_interface import *
+from .parmed_interface import *
