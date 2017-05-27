9996bf895a3a769e59b7936a886211b3b3678ce6
Aaron Virshup <aaron.virshup@autodesk.com>
2017-05-26 18:08:07 -0700

    Databases working in py2 and py3

--- moldesign/utils/databases.py
+++ moldesign/utils/databases.py
@@ -1,3 +1,8 @@
+from __future__ import print_function, absolute_import, division
+from future.builtins import *
+from future import standard_library
+standard_library.install_aliases()
+
 # Copyright 2017 Autodesk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,20 +16,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from future import standard_library
-standard_library.install_aliases()
-from builtins import object
-import dbm.dumb
+import sys
 import json
 import zlib
 
 from . import Alias
 
+if sys.version_info.major == 2:
+    import dumbdbm
+else:
+    import dbm.dumb as dumbdbm
+    assert sys.version_info.major >= 3
 
 class CompressedJsonDbm(object):
     """ Quick-and-dirty interface to a DBM file
     """
-    def __init__(self, filename, flag='r', dbm=dbm.dumb):
+    def __init__(self, filename, flag='r', dbm=dumbdbm):
         self.dbm = dbm
         if hasattr(dbm, 'open'):
             self.db = self.dbm.open(filename, flag)
@@ -51,7 +58,7 @@ class CompressedJsonDbm(object):
     __contains__ = Alias('db.__contains__')
 
 
-class ReadOnlyDumb(dbm.dumb._Database):
+class ReadOnlyDumb(dumbdbm._Database):
     """ A read-only subclass of dumbdbm
 
     All possible operations that could result in a disk write have been turned into no-ops or raise
@@ -69,55 +76,5 @@ class ReadOnlyDumb(dbm.dumb._Database):
 
     def _addkey(self, *args):
         assert False, 'Should never be here - this is a read-only database'
-
-
-class Hdf5Dbm(object):
-    """ A simplified DBM interface backed by HDF5.
-
-    Like DBMs, it only stores strings. Unlike DBMs, the strings must be completely ascii
-
-    Note:
-        if something other than a string is passed, it will be converted using json.dumps
-
-    This was written only because dumbdbm has a tendency to get corrupted (because it doesn't
-    support read-only mode), none of the other standard library DBMs are portable, and other
-    alternatives (like semidbm) don't play nice with git.
-    """
-    def __init__(self, filename, mode='r', compression=10):
-        """Initialization:
-
-        Args:
-            filename (str): path to file (including suffix)
-            mode (str): ``mode`` argument for ``h5py.File``
-            compression (str or int): compression for datasets (if int: gzip level)
-        """
-        import h5py
-
-        self.f = h5py.File(filename, mode)
-        self._strtype = h5py.special_dtype(vlen=bytes)
-        self.compression = compression
-
-    @classmethod
-    def open(cls, *args, **kwargs):
-        """Synonym for __init__ for compatibility with stdlib DBM modules"""
-        return cls(*args, **kwargs)
-
-    def __getitem__(self, item):
-        ds = self.f[item]
-        return json.loads(ds.value)
-
-    def __setitem__(self, item, value):
-        jsval = json.dumps(value)
-        if item in self.f:
-            del self.f[item]
-        self.f.create_dataset(item, data=jsval,
-                              dtype=self._strtype, compression=self.compression)
-
-    def close(self):
-        return self.f.close()
-
-
-
-
 
 
