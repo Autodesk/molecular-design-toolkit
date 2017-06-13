fd4d8e2c934be500a673475941dc92a6ee50ed49
Aaron Virshup <avirshup@gmail.com>
2017-06-12 20:17:37 -0700

    oops

--- deployment/publish.sh
+++ deployment/publish.sh
@@ -11,7 +11,6 @@ pyversion=$(python -m moldesign version | tail -n 1)
 
 if [ "${pyversion}" == "${CI_BRANCH}" ]; then
   echo "Deploying version ${CI_BRANCH}"
-  exit 0
 else
   echo "Can't publish - moldesign package version '${pyversion}' differs from its Git tag '${CI_BRANCH}'"
   exit 1
