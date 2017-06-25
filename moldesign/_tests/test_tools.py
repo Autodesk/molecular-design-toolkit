fixes pmi calculation, tests it

moldesign/_tests/test_tools.py
@@ -9,7 +9,8 @@ import numpy as np
 import moldesign as mdt
 from moldesign import units as u
 
-from .helpers import get_data_path
+from .helpers import get_data_path, assert_almost_equal
+from .molecule_fixtures import ligand3aid,pdb3aid,benzene,small_molecule,pdb1yu8, ligand_residue_3aid
 
 
 registered_types = {}
@@ -27,46 +28,79 @@ def typedfixture(*types, **kwargs):
     return fixture_wrapper
 
 
-@pytest.fixture(scope='function')
-def linear_mol_and_pmi():
-    mol = mdt.from_smiles('C#CC#CC#C')
+
+@pytest.mark.parametrize('fixturename', 'ligand3aid pdb3aid benzene small_molecule pdb1yu8'.split())
+def test_pmi_is_orthonormal(request, fixturename):
+    # test a bunch of molecules
+    mol = request.getfixturevalue(fixturename)
     pmi = mdt.tools.PrincipalMomentsOfInertia(mol)
-    return mol, pmi
-
-
-def test_pmi_orthonormal(linear_mol_and_pmi):
-    pmi, mol = linear_mol_and_pmi
     for ivec in range(3):
         assert abs(1.0 - mdt.mathutils.norm(pmi.evecs[ivec])) < 1e-12
-        for jvec in range(ivec, 3):
+        for jvec in range(ivec+1, 3):
             assert abs(pmi.evecs[ivec].dot(pmi.evecs[jvec])) < 1e-12
 
 
-def test_principal_moment_of_inertia_reorientation(linear_mol_and_pmi):
-    # note that this will fail if the generated polymer is not perfectly linear
-    mol, pmi = linear_mol_and_pmi
+@pytest.mark.parametrize('fixturename', 'ligand3aid pdb3aid benzene small_molecule pdb1yu8'.split())
+def test_pmi_translational_rotational_invariance(request, fixturename):
+    # currently only test the eigenvalues
+    mol = request.getfixturevalue(fixturename)
+    pmi = mdt.tools.PrincipalMomentsOfInertia(mol)
 
+    _randomize_orientation(mol)
+    pmi2 = mdt.tools.PrincipalMomentsOfInertia(mol)
+    assert_almost_equal(pmi.moments, pmi2.moments)
+
+
+def _randomize_orientation(mol):
+    translation = 10.0*(np.random.rand(3)-0.5)*u.angstrom
+    mol.translate(translation)
+
+    axis = [random.uniform(-1.0, 1.0),
+            random.uniform(-1.0, 1.0),
+            random.uniform(0.4, 1.0)]  # make sure to rotate off of x-axis
+    mol.rotate(axis=axis, angle=random.randrange(15.0, 170.0) * u.degrees)
+
+
+def test_pmi_reorientation_on_linear_molecule():
+    # note that this will fail if the generated polymer is not perfectly linear
+    mol = mdt.from_smiles('C#CC#CC#C')
     original_distmat = mol.calc_distance_array().defunits_value()
 
-    for deg in np.linspace(10, 120, 6) * u.degree:
-        # Set up a new random reorientation every time
-        axis = [random.uniform(-1.0, 1.0),
-                random.uniform(-1.0, 1.0),
-                random.uniform(0.4, 1.0)]  # make sure to rotate off of x-axis
-        translation = 10.0 * (np.random.rand(3)-0.5) * u.angstrom
-        mol.translate(translation)
-        mol.rotate(axis=axis, angle=deg)
+    for i in range(5):
+        _randomize_orientation(mol)
 
         # calculate PMI and reorient
         pmi = mdt.tools.PrincipalMomentsOfInertia(mol)
         pmi.reorient(mol)
 
-        # Check that everything lies along the x-axis
-        np.testing.assert_allclose(mol.positions[:,1:], 0.0)
+        # Check that everything lies along the z-axis
+        np.testing.assert_allclose(mol.positions[:,:2], 0.0, atol=1e-12)
 
         # Check that distance matrix was unchanged under the transformation
         newdistmat = mol.calc_distance_array()
-        np.testing.assert_allclose(newdistmat.defunits_value,
+        np.testing.assert_allclose(newdistmat.defunits_value(),
+                                   original_distmat)
+
+
+def test_pmi_reorientation_on_benzene(benzene):
+    # note that this will fail if the generated polymer is not perfectly linear
+    mol = benzene
+    original_distmat = mol.calc_distance_array().defunits_value()
+
+    for i in range(5):
+        _randomize_orientation(mol)
+
+        # calculate PMI and reorient
+        pmi = mdt.tools.PrincipalMomentsOfInertia(mol)
+        pmi.reorient(mol)
+
+        # Check that molecule is _roughly_ in x-y plane
+        np.testing.assert_allclose(mol.positions[:,0].value_in(u.angstrom), 0.0,
+                                   atol=0.1)
+
+        # Check that distance matrix was unchanged under the transformation
+        newdistmat = mol.calc_distance_array()
+        np.testing.assert_allclose(newdistmat.defunits_value(),
                                    original_distmat)
 
 

moldesign/tools/eigen.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, absolute_import, division
 from future.builtins import *
 from future import standard_library
-
 standard_library.install_aliases()
 
 # Copyright 2017 Autodesk Inc.
@@ -41,7 +40,7 @@ class Eigenspace(object):
         It's generally assumed that Eigen space objects will be subclassed to wrap various bits of
         eignproblem-related functionality.
     """
-    def __init__(self, evals, evecs):
+    def __init__(self, evals, evecs, real=True):
         if len(evals) != len(evecs):
             raise ValueError('Eigenvalues and eigenvectors have different lengths!')
 
@@ -77,7 +76,8 @@ class Eigenspace(object):
             def keyfn(t):
                 return key(t[0], t[1])
 
-        evals, evecs = zip(*sorted(zip(self.evals, self.evecs), key=keyfn, reverse=largest_first))
+        evals, evecs = zip(*sorted(zip(self.evals.copy(), self.evecs.copy()),
+                                   key=keyfn, reverse=largest_first))
         self.evals[:] = evals
         self.evecs[:] = evecs
 
@@ -104,8 +104,8 @@ class Eigenspace(object):
 class PrincipalMomentsOfInertia(Eigenspace):
     """ Calculates the moments of inertia of a molecule at a given position
 
-    Note:
-        this object is NOT updated if the initializing molecule's positions change.
+    Notes:
+        This object is NOT updated if the initializing molecule's positions change.
         This allows it to be used to reorient other molecules into the same coordinate system
 
     Args:
@@ -113,8 +113,8 @@ class PrincipalMomentsOfInertia(Eigenspace):
         mass_centered (bool): calculate PMIs relative to molecule's center of mass (default: True)
 
     Attributes:
-        evals (Vector[length**2 * mass]): eigenvalues of the principal moments
-        evecs (List[Vector]]): normalized principal moment vectors
+        axes (List[Vector[len=3]]]): principal rotation axes
+        moments (Vector[length**2 * mass, len=3]): rotational inertias for the three axes
     """
     def __init__(self, mol, mass_centered=True):
         from scipy.linalg import eig
@@ -123,9 +123,20 @@ class PrincipalMomentsOfInertia(Eigenspace):
         evals, evecs = eig(inertia_tensor.defunits_value())  # strip units before going to scipy
         evals = evals*u.default.mass*u.default.length ** 2  # reapply units to evals
 
-        super().__init__(evals, evecs.T)
+        assert (abs(evecs.imag) < 1e-12).all()  # hopefully everything is real?
+        assert (abs(evals.imag) < 1e-12 * evals.units).all()
+
+        super().__init__(evals.real, evecs.T)
         self._com = com
         self.sort()
+
+    @property
+    def moments(self):
+        return self.evals
+
+    @property
+    def axes(self):
+        return self.evecs
 
     def reorient(self, mol):
         """ Rotates molecular coordinates into the PMI coordinate system
@@ -134,29 +145,36 @@ class PrincipalMomentsOfInertia(Eigenspace):
         y along PMI 2, and z along PMI 3.
 
         Args:
-            mol (moldesign.Molecule): molecule to re-orient - its current positinos will be changed.
+            mol (moldesign.Molecule): molecule to re-orient - its current positinos will be changed
         """
         if self._com is not None:
             mol.com -= self._com
         mol.positions = self.transform(mol.positions)
 
 
-
 @exports
 def get_inertia_tensor(mol, mass_centered=True):
+    """ Calculates the moment of inertia for a molecule
+
+    Args:
+        mol (moldesign.Molecule): calculate the inertia tensor of this  molecule
+        mass_centered (bool): if True, calcualte tensor relative to molecule's center of mass. If
+           False, calculate it relative to the current origin.
+
+    Returns:
+        Matrix[mass*length*length]: moment of inertia tensor
+    """
     inertia_tensor = np.zeros((3,3)) * u.default.mass * u.default.length**2
     com = mol.com if mass_centered else None
-    for atom in mol.atoms:
-        pos = atom.position.copy()
-        if mass_centered:
-            pos -= mol.com
 
-        for i in range(3):
-            inertia_tensor[i,i] += atom.mass * (pos[i-1]**2 + pos[i-2]**2)
-            for j in range(i+1,3):
-                di = atom.mass * (pos[i] * pos[j])
-                inertia_tensor[i,j] += di
-                inertia_tensor[j,i] += di
+    pos = mol.positions
+    if mass_centered:
+        pos = pos - mol.com
+
+    for i in range(3):
+        inertia_tensor[i,i] = (mol.masses * (pos[:,i-1]**2 + pos[:,i-2]**2)).sum()
+        for j in range(i+1,3):
+            di = (mol.masses * pos[:,i] * pos[:,j]).sum()
+            inertia_tensor[i,j] = -di
+            inertia_tensor[j,i] = -di
     return inertia_tensor, com
-
-