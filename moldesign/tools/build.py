5f4c3f843f296956e0a1804c63a204c7301b7212
Dave Parker <dave.parker@autodesk.com>
2017-09-21 12:01:07 -0700

    Add a new function build_model() to build a structural model from a template and target sequence.

--- moldesign/tools/build.py
+++ moldesign/tools/build.py
@@ -84,3 +84,77 @@ def build_assembly(mol, assembly_name):
     newmol = mdt.Molecule(all_atoms,
                           name="%s (bioassembly %s)" % (mol.name, assembly_name))
     return newmol
+
+@toplevel
+def build_model(temp_molecule, temp_chain_id, temp_start, temp_end, temp_sequence,
+        targ_start, targ_end, targ_sequence): 
+    """ Build a model of a structure for a given target sequence from a 
+        template sequence and structure.
+
+    Note that there is often a discrepancy between the number of residues in the temp
+    sequence and the number of actual residues contained in the molecule. 
+
+    Args:
+        temp_molecule(moldesign.Molecule): The molecule used as a template structure. 
+        temp_chain_id(string): The ID of the chain in the template structure. 
+        temp_start(int): The start of the template sequence. 
+        temp_end(int): The end of the template sequence. 
+        temp_sequence(string): The 1 character amino acid sequence of the template.
+        targ_start(int): The start of the target sequence. 
+        targ_end(int): The end of the target sequence. 
+        targ_sequence(string): The 1 charcter amino acid sequence of the target. 
+
+    Returns:
+        moldesign.Molecule: Molecule representing a structural model.
+    """
+    if temp_chain_id not in temp_molecule.chains:
+        raise ValueError("No chain '%s' found in molecule '%s'" % 
+             (temp_chain_id,temp_molecule.name))
+
+    chain = temp_molecule.chains[temp_chain_id]
+    residues = chain.residues
+    atoms = chain.atoms
+
+    # Add residues from the template sequence that are present in the template
+    # molecule and create a list of mutations where they differ from the target 
+    # sequence. 
+    mutation_list = []
+    temp_residues = []
+    temp_seq_num = temp_start
+    targ_seq_num = targ_start
+    for temp_aa, targ_aa in zip(temp_sequence, targ_sequence):
+        temp_res_name = mdt.data.mdt.data.RESIDUE_CODE_TO_NAME[temp_aa] + str(temp_seq_num)
+        #print("mdt.build_model] temp_res_name %s" % temp_res_name) 
+        if temp_res_name in residues:
+            if temp_aa != targ_aa:
+                #mutate_str = "%s.%s%d%s" % (temp_chain_id, temp_aa, targ_seq_num, targ_aa)
+                mutate_str = "%s.%s%d%s" % (temp_chain_id, temp_aa, residues[temp_res_name].pdbindex, targ_aa)
+                mutation_list.append(mutate_str)
+            temp_res = residues[temp_res_name]
+            #temp_res.pdbindex = targ_seq_num 
+            temp_residues.append(temp_res)
+        temp_seq_num += 1
+    #__for temp_aa, targ_aa in zip(temp_sequence, targ_sequence)
+
+    # Create a template molecule from the template sequence. 
+    temp_molecule = mdt.Molecule(temp_residues)
+    if len(mutation_list) == 0:
+        return temp_molecule
+
+    # Change residues in template molecule to residues of the target sequence.
+    model_molecule = mdt.mutate_residues(temp_molecule, mutation_list)
+
+    # Check that the number of bonds for backbone atoms match.
+    for res, model_res in zip(temp_molecule.residues, model_molecule.residues):
+        if not res.backbone:
+            continue
+        for atom in res.backbone:
+            bonds = [bond for bond in temp_molecule.bond_graph[atom] if bond.name in res.backbone]
+            model_atom = model_molecule.chains[temp_chain_id].residues[model_res.name].atoms[atom.name]
+            model_bonds = [bond for bond in model_molecule.bond_graph[model_atom] \
+                if bond.name in model_res.backbone]
+            if len(bonds) != len(model_bonds):
+                raise Exception("Backbone atom bonds are missing")
+
+    return model_molecule 
+
