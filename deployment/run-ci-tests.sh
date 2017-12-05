3ce8e27ba6ec5b1f33e4239262f32c6a737ee7f6
Aaron Virshup <avirshup@gmail.com>
2017-12-04 16:08:58 -0800

    Rename commit message flags to avoid clashes

--- deployment/run-ci-tests.sh
+++ deployment/run-ci-tests.sh
@@ -32,12 +32,12 @@ function send_status_update(){
 function check_if_tests_should_run(){
    echo "Should I run the tests in this environment?"
 
-   if [[ "${CI_COMMIT_MESSAGE}" == *"--skip-ci-tests"* ]];  then
+   if [[ "${CI_COMMIT_MESSAGE}" == *"--no-tests"* ]];  then
        echo "NO: found \"--skip-ci-tests\" flag in commit message; will not run any test suites"
        exit 0
    fi
 
-   if [[ "${CI_COMMIT_MESSAGE}" == *"--fast-ci-tests"* && "${VERSION}" != "complete.py3" ]];  then
+   if [[ "${CI_COMMIT_MESSAGE}" == *"--fast-tests"* && "${VERSION}" != "complete.py3" ]];  then
        echo "NO: found \"--fast-ci-tests\" flag in commit message; run complete.py3 only"
        exit 0
    fi
