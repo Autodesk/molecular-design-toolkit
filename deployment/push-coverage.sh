37e6035665fdba5ab4ef78ef3179394ebdfc800c
Aaron Virshup <avirshup@gmail.com>
2017-12-05 18:08:32 -0800

    Fix coverage pusher better

--- deployment/push-coverage.sh
+++ deployment/push-coverage.sh
@@ -4,9 +4,8 @@
 set -e
 
 mkdir -p /opt/reports/env-coverage
-cp /opt/reports/env-coverage/coverage.* /opt/reports/
-
-if $(coverage combine /opt/reports/coverage.*); then
+if $(cp /opt/reports/env-coverage/coverage.* /opt/reports/); then
+  coverage combine /opt/reports/coverage.*
   coveralls || echo "Failed to upload coverage to coveralls.io"
 else
    echo "No coverage files found, skipping coveralls upload"
